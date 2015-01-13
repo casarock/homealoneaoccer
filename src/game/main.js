1e9bf82a76b57fc39d039ff157732a4bae31ec85
Carsten Sandtner <sandtner@mediaman.de>
2015-01-13 15:31:36 +0100

    add click to start and Score

--- src/game/main.js
+++ src/game/main.js
@@ -10,20 +10,47 @@ game.module(
 	game.createScene('Main', {
 		gravity: 1200,
 		backgroundColor: 0x47c7ea,
+		gameRuns: false,
+		score: 0,
+		scoreText: null,
 
 		init: function() {
 			this.world = new game.World(0, this.gravity);
-
 			this.wall = new game.Wall(game.system.width - 17, game.system.height - 256 - 64);
 			this.ground = new game.Ground(0, game.system.height - 64);
-
 			this.player = new game.Player(50, game.system.height - 36);
-			this.ball = new game.Ball(232, 200);
+
+			this.scoreText = new game.BitmapText("Score:" + this.pad(this.score, 3, 0), { font: 'font', align: 'left' });
+			this.scoreText.position.x = 10;
+			this.scoreText.position.y = game.system.height - 26;
+			this.scoreText.addTo(this.stage);
 		},
 
 		mousedown: function() {
-			this.player.jump();
+			if (this.gameRuns) {
+				this.player.jump();
+			} else {
+				this.ball = new game.Ball(20, 200);
+				this.gameRuns = true;
+			}
+		},
+
+		addScore: function() {
+			this.score++;
+
+			console.log(this.score);
+			this.scoreText.setText("Score:" + this.pad(this.score, 3, 0));
+			this.scoreText.updateTransform();
+			this.scoreText.position.x = 10;
+			this.scoreText.position.y = game.system.height - 26;
+		},
+
+		pad: function(n, width, z) {
+			z = z || '0';
+			n = n + '';
+			return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 		}
+
 	});
 
 });
